eb136135a09690f02b0083a87fc4e0dcdf72b9d2
TTTrouble <mannevijay@gmail.com>
2024-07-15 03:55:05 -0400

    added a comment to test source control

--- server/server.js
+++ server/server.js
@@ -1,39 +1,44 @@
-var express = require('express');
-var bodyParser = require('body-parser');
+var express = require("express");
+var bodyParser = require("body-parser");
 
-var { mongoose } = require('./db/mongoose');
-var { User } = require('./models/users');
-var { Todo } = require('./models/todo');
-
+var { mongoose } = require("./db/mongoose");
+var { User } = require("./models/users");
+var { Todo } = require("./models/todo");
+//this is a comment now isn't it!
 var app = express();
 
 app.use(bodyParser.json());
 
-app.post('/todos', (req, res) => {
-    console.log(req.body);
+app.post("/todos", (req, res) => {
+  console.log(req.body);
 
-    var todo = new Todo({
-        text: req.body.text
-    });
+  var todo = new Todo({
+    text: req.body.text,
+  });
 
-    todo.save().then(doc => {
-        res.send(doc);
-    }, e => {
-        res.status(400).send(e);
-    });
+  todo.save().then(
+    (doc) => {
+      res.send(doc);
+    },
+    (e) => {
+      res.status(400).send(e);
+    }
+  );
 });
 
-app.get('/todos', (req, res) => {
-    Todo.find().then((todos) => {
-        res.send({todos});
-    }, (e) => {
-        res.status(400).send(e);
-    });
+app.get("/todos", (req, res) => {
+  Todo.find().then(
+    (todos) => {
+      res.send({ todos });
+    },
+    (e) => {
+      res.status(400).send(e);
+    }
+  );
 });
-
 
 app.listen(process.env.PORT, () => {
-    console.log(`Started on port ${process.env. PORT}`);
+  console.log(`Started on port ${process.env.PORT}`);
 });
 
-module.exports = {app};
+module.exports = { app };
